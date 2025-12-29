4c5d2c3bf1aa4dce08e99c029c76b94e92a8e136
PhyBruno <phybruno@users.noreply.github.com>
2025-12-29 03:40:41 -0300

    feat: v1.0.97 - KB Item Creation

--- nodes/GlpiApi/resources/Tool Management/create.ts
+++ nodes/GlpiApi/resources/Tool Management/create.ts
@@ -14,5 +14,66 @@ export const toolManagementCreateDescription: INodeProperties[] = [
 		},
 		description: 'Create a new item',
 	},
-	// Future UI fields will be added here
+	// Knowledge Base Item Fields
+	{
+		displayName: 'Subject',
+		name: 'name',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['Tool Management'],
+				operation: ['create'],
+				itemtype: ['KnowbaseItem'],
+			},
+		},
+		description: 'Subject of the knowledge base item',
+	},
+	{
+		displayName: 'Content',
+		name: 'answer',
+		type: 'string',
+		default: '',
+		required: true,
+		typeOptions: {
+			alwaysOpenEditWindow: true,
+		},
+		displayOptions: {
+			show: {
+				resource: ['Tool Management'],
+				operation: ['create'],
+				itemtype: ['KnowbaseItem'],
+			},
+		},
+		description: 'Content of the knowledge base item',
+	},
+	{
+		displayName: 'Knowledgebase Category ID',
+		name: 'knowbaseitems_categories_id',
+		type: 'number',
+		default: 0,
+		displayOptions: {
+			show: {
+				resource: ['Tool Management'],
+				operation: ['create'],
+				itemtype: ['KnowbaseItem'],
+			},
+		},
+		description: 'ID of the knowledge base category',
+	},
+	{
+		displayName: 'Put This Item in the FAQ',
+		name: 'is_faq',
+		type: 'boolean',
+		default: false,
+		displayOptions: {
+			show: {
+				resource: ['Tool Management'],
+				operation: ['create'],
+				itemtype: ['KnowbaseItem'],
+			},
+		},
+		description: 'Whether to include this item in the FAQ',
+	},
 ];
