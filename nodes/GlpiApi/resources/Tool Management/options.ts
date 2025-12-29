e5958a88dfe403724b6087adf7605c637431c94e
PhyBruno <phybruno@users.noreply.github.com>
2025-12-29 03:43:06 -0300

    feat: v1.0.98 - Create Project

--- nodes/GlpiApi/resources/Tool Management/options.ts
+++ nodes/GlpiApi/resources/Tool Management/options.ts
@@ -14,7 +14,94 @@ export const toolManagementOptionsDescription: INodeProperties[] = [
 			},
 		},
 		options: [
-			// Future optional fields will be added here
+			// Project Options
+			{
+				displayName: 'Code',
+				name: 'code',
+				type: 'string',
+				default: '',
+				displayOptions: {
+					show: {
+						itemtype: ['Project'],
+					},
+				},
+				description: 'Code of the project',
+			},
+			{
+				displayName: 'Comments',
+				name: 'comment',
+				type: 'string',
+				default: '',
+				typeOptions: {
+					alwaysOpenEditWindow: true,
+				},
+				displayOptions: {
+					show: {
+						itemtype: ['Project'],
+					},
+				},
+				description: 'Comments for the project',
+			},
+			{
+				displayName: 'Group ID',
+				name: 'groups_id',
+				type: 'number',
+				default: 0,
+				displayOptions: {
+					show: {
+						itemtype: ['Project'],
+					},
+				},
+				description: 'ID of the group',
+			},
+			{
+				displayName: 'Manager User ID',
+				name: 'users_id',
+				type: 'number',
+				default: 0,
+				displayOptions: {
+					show: {
+						itemtype: ['Project'],
+					},
+				},
+				description: 'ID of the manager user',
+			},
+			{
+				displayName: 'Priority',
+				name: 'priority',
+				type: 'number',
+				default: 3,
+				displayOptions: {
+					show: {
+						itemtype: ['Project'],
+					},
+				},
+				description: 'Priority of the project',
+			},
+			{
+				displayName: 'State ID',
+				name: 'projectstates_id',
+				type: 'number',
+				default: 0,
+				displayOptions: {
+					show: {
+						itemtype: ['Project'],
+					},
+				},
+				description: 'ID of the project state',
+			},
+			{
+				displayName: 'Type ID',
+				name: 'projecttypes_id',
+				type: 'number',
+				default: 0,
+				displayOptions: {
+					show: {
+						itemtype: ['Project'],
+					},
+				},
+				description: 'ID of the project type',
+			},
 		],
 	},
 ];
